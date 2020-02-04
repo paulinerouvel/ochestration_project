991eb54b8fe0b240bdb9b5734e19568f7e4215cd
Pauline Rouvel <pauline.rouvel@engie.com>
2020-02-04 14:00:29 +0100

    add exit4

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require("supertest");
 var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
+ //navigate to root and check the the response is "hello world"e
  request(app).get('/').expect('Hello World!', done);
  });
 });
