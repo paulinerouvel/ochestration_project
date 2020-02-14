0236f164b98cf1c7c6a52a62f8b52ea75c39f7ef
Pauline Rouvel <pauline.rouvel@engie.com>
2020-02-14 13:49:30 +0100

    change test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Hello World!');
+ res.send('HelloWorld!');
  res.end();
 });
 //listen to port 3000 by default
